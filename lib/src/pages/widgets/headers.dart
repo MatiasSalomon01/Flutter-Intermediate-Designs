22e13566056f3961e9afc6ccb2c8033cae432f01
MatiasSalomon01 <matisalomond@gmail.com>
2023-03-18 20:57:11 -0300

    Header Diagonal

--- lib/src/pages/widgets/headers.dart
+++ lib/src/pages/widgets/headers.dart
@@ -28,3 +28,46 @@ class HeaderBordersRedondeados extends StatelessWidget {
     );
   }
 }
+
+class HeaderDiagonal extends StatelessWidget {
+  const HeaderDiagonal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      // color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = Paint();
+
+    //Propiedades
+    lapiz.color = Color(0xff615AAB);
+    // lapiz.style = PaintingStyle.stroke; //para dibujar bordes
+    lapiz.style = PaintingStyle.fill; //para rellenar los bordes
+    lapiz.strokeWidth = 2;
+
+    final path = new Path();
+
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.30);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
