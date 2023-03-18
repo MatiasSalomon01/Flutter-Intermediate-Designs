9c926f7f10edfc5f19df6494b887d96a58328761
MatiasSalomon01 <matisalomond@gmail.com>
2023-03-18 20:35:22 -0300

    Header Circular

--- lib/src/pages/widgets/headers.dart
+++ lib/src/pages/widgets/headers.dart
@@ -11,3 +11,20 @@ class HeaderCuadrado extends StatelessWidget {
     );
   }
 }
+
+class HeaderBordersRedondeados extends StatelessWidget {
+  const HeaderBordersRedondeados({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      //color: Color(0xff615AAB),
+      decoration: BoxDecoration(
+          color: Color(0xff615AAB),
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(30),
+              bottomRight: Radius.circular(30))),
+    );
+  }
+}
