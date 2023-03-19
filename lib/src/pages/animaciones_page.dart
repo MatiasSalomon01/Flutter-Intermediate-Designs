00fcfa987ce3a7ee3a54ea964762fadc6c7248bf
MatiasSalomon01 <matisalomond@gmail.com>
2023-03-19 00:17:38 -0300

    Preparando la pantalla para animar

---
+++ lib/src/pages/animaciones_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class AnimacionesPage extends StatelessWidget {
+  const AnimacionesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CuadradoAnimado(),
+      ),
+    );
+  }
+}
+
+class CuadradoAnimado extends StatefulWidget {
+  const CuadradoAnimado({
+    super.key,
+  });
+
+  @override
+  State<CuadradoAnimado> createState() => _CuadradoAnimadoState();
+}
+
+class _CuadradoAnimadoState extends State<CuadradoAnimado> {
+  @override
+  Widget build(BuildContext context) {
+    return _Rectangulo();
+  }
+}
+
+class _Rectangulo extends StatelessWidget {
+  const _Rectangulo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      width: 70,
+      decoration: BoxDecoration(color: Colors.blue),
+    );
+  }
+}
