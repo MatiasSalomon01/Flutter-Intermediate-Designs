01e3841a73621855a3622dad9851b4865f8c0251
MatiasSalomon01 <matisalomond@gmail.com>
2023-03-18 21:12:20 -0300

    Header Curvo

--- lib/src/pages/widgets/headers.dart
+++ lib/src/pages/widgets/headers.dart
@@ -156,3 +156,46 @@ class _HeaderPicoPainter extends CustomPainter {
     return true;
   }
 }
+
+class HeaderCurvo extends StatelessWidget {
+  const HeaderCurvo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      // color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderCurvoPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderCurvoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = Paint();
+
+    //Propiedades
+    lapiz.color = Color(0xff615AAB);
+    // lapiz.style = PaintingStyle.stroke; //para dibujar bordes
+    lapiz.style = PaintingStyle.fill; //para rellenar los bordes
+    lapiz.strokeWidth = 20;
+
+    final path = new Path();
+
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(
+        size.width / 2, size.height * 0.4, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
