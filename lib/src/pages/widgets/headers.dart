3deab382bd1841e10d1162d13c89ee0324555a48
MatiasSalomon01 <matisalomond@gmail.com>
2023-03-18 21:01:56 -0300

    Header Triangulo

--- lib/src/pages/widgets/headers.dart
+++ lib/src/pages/widgets/headers.dart
@@ -71,3 +71,45 @@ class _HeaderDiagonalPainter extends CustomPainter {
     return true;
   }
 }
+
+class HeaderTriangular extends StatelessWidget {
+  const HeaderTriangular({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      // color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderTriangularPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderTriangularPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = Paint();
+
+    //Propiedades
+    lapiz.color = Color(0xff615AAB);
+    // lapiz.style = PaintingStyle.stroke; //para dibujar bordes
+    lapiz.style = PaintingStyle.fill; //para rellenar los bordes
+    lapiz.strokeWidth = 2;
+
+    final path = new Path();
+
+    path.moveTo(0, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
