426e1f082cc106697539f7fbf74ff2129ecced8a
=Matias Salomon <matias.salomon@clt.com.py>
2023-03-20 09:48:40 -0300

    Animar en intervalos de tiempo

--- lib/src/pages/animaciones_page.dart
+++ lib/src/pages/animaciones_page.dart
@@ -38,7 +38,8 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>
     rotacion = Tween(begin: 0.0, end: 2 * math.pi)
         .animate(CurvedAnimation(parent: controller, curve: Curves.easeOut));
 
-    opacidad = Tween(begin: 0.1, end: 1.0).animate(controller);
+    opacidad = Tween(begin: 0.1, end: 1.0).animate(CurvedAnimation(
+        parent: controller, curve: Interval(0, 0.75, curve: Curves.easeOut)));
 
     controller.addListener(() {
       print('Status: ${controller.status}');
