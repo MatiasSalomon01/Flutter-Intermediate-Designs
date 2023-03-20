0347779a43c6f78a094c9676f7b0da89b6159f7c
=Matias Salomon <matias.salomon@clt.com.py>
2023-03-20 15:58:53 -0300

    PageView - Creando un Slideshow

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -8,8 +8,44 @@ class SlideShowPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SvgPicture.asset('assets/svgs/slide-1.svg'),
+        child: _Slides(),
       ),
+    );
+  }
+}
+
+//SvgPicture.asset('assets/svgs/slide-1.svg'),
+
+class _Slides extends StatelessWidget {
+  const _Slides({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        children: [
+          _Slide('assets/svgs/slide-1.svg'),
+          _Slide('assets/svgs/slide-2.svg'),
+          _Slide('assets/svgs/slide-3.svg'),
+          _Slide('assets/svgs/slide-4.svg'),
+          _Slide('assets/svgs/slide-5.svg'),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svg;
+  _Slide(this.svg);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: EdgeInsets.all(30),
+      child: SvgPicture.asset(svg),
     );
   }
 }
