3e06e2e19f7ee0378b2f7edcdd84cb9233563259
=Matias Salomon <matias.salomon@clt.com.py>
2023-03-20 13:49:13 -0300

    Animando el arco

--- lib/src/pages/circular_progress_page.dart
+++ lib/src/pages/circular_progress_page.dart
@@ -4,10 +4,8 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class CircularProgressPage extends StatefulWidget {
-  const CircularProgressPage({super.key});
-
   @override
-  State<CircularProgressPage> createState() => _CircularProgressPageState();
+  _CircularProgressPageState createState() => _CircularProgressPageState();
 }
 
 class _CircularProgressPageState extends State<CircularProgressPage>
@@ -19,12 +17,11 @@ class _CircularProgressPageState extends State<CircularProgressPage>
 
   @override
   void initState() {
-    controller =
-        AnimationController(vsync: this, duration: Duration(microseconds: 800));
+    controller = new AnimationController(
+        vsync: this, duration: Duration(milliseconds: 800));
 
     controller.addListener(() {
-      // print('Valor Controller: ${controller.value}');
-
+      // print('valor controller: ${ controller.value }');
       setState(() {
         porcentaje = lerpDouble(porcentaje, nuevoPorcentaje, controller.value)!;
       });
@@ -43,19 +40,20 @@ class _CircularProgressPageState extends State<CircularProgressPage>
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          porcentaje = nuevoPorcentaje;
-          nuevoPorcentaje += 10;
-          if (nuevoPorcentaje > 100) {
-            nuevoPorcentaje = 0;
-            porcentaje = 0;
-          }
-          controller.forward(from: 0.0);
-          setState(() {});
-        },
-        child: Icon(Icons.refresh),
-        backgroundColor: Colors.indigo,
-      ),
+          child: Icon(Icons.refresh),
+          backgroundColor: Colors.pink,
+          onPressed: () {
+            porcentaje = nuevoPorcentaje;
+            nuevoPorcentaje += 10;
+            if (nuevoPorcentaje > 100) {
+              nuevoPorcentaje = 0;
+              porcentaje = 0;
+            }
+
+            controller.forward(from: 0.0);
+
+            setState(() {});
+          }),
       body: Center(
         child: Container(
           padding: EdgeInsets.all(5),
@@ -75,32 +73,32 @@ class _MiRadialProgress extends CustomPainter {
   final porcentaje;
 
   _MiRadialProgress(this.porcentaje);
+
   @override
   void paint(Canvas canvas, Size size) {
-    //Circulo Completado
-    final paint = Paint()
+    // Circulo completado
+    final paint = new Paint()
       ..strokeWidth = 4
       ..color = Colors.grey
       ..style = PaintingStyle.stroke;
 
-    final center = Offset(size.width * 0.5, size.height * 0.5);
+    final center = new Offset(size.width * 0.5, size.height / 2);
     final radio = min(size.width * 0.5, size.height * 0.5);
+
     canvas.drawCircle(center, radio, paint);
 
-    //Arco
-    final paintArco = Paint()
+    // Arco
+    final paintArco = new Paint()
       ..strokeWidth = 10
       ..color = Colors.pink
       ..style = PaintingStyle.stroke;
 
-    //Parte a rellenar
+    // Parte que se deberá ir llenando
     double arcAngle = 2 * pi * (porcentaje / 100);
     canvas.drawArc(Rect.fromCircle(center: center, radius: radio), -pi / 2,
         arcAngle, false, paintArco);
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
-  }
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
