9be7937f7c4d3eeccff7712d22a068f5d7bced38
=Matias Salomon <matias.salomon@clt.com.py>
2023-03-20 10:19:08 -0300

    Inicio de un nuevo reto

---
+++ lib/src/challenges/cuadrado_animado_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class CuadradoAnimadoPage extends StatelessWidget {
+  const CuadradoAnimadoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: _CuadradoAnimado()),
+    );
+  }
+}
+
+class _CuadradoAnimado extends StatefulWidget {
+  @override
+  State<_CuadradoAnimado> createState() => _CuadradoAnimadoState();
+}
+
+class _CuadradoAnimadoState extends State<_CuadradoAnimado> {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _Rectangulo();
+  }
+}
+
+class _Rectangulo extends StatelessWidget {
+  const _Rectangulo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(color: Colors.blue),
+    );
+  }
+}
