e9a5805b2cb5b47dfa1b8fc3a350a0d6985966cf
=Matias Salomon <matias.salomon@clt.com.py>
2023-03-20 10:11:27 -0300

    Tarea: FadeOut

--- lib/src/pages/animaciones_page.dart
+++ lib/src/pages/animaciones_page.dart
@@ -29,6 +29,7 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>
 
   late Animation<double> rotacion;
   late Animation<double> opacidad;
+  late Animation<double> opacidadOut;
   late Animation<double> moverDerecha;
   late Animation<double> agrandar;
 
@@ -42,6 +43,9 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>
 
     opacidad = Tween(begin: 0.1, end: 1.0).animate(CurvedAnimation(
         parent: controller, curve: Interval(0, 0.75, curve: Curves.easeOut)));
+
+    opacidadOut = Tween(begin: 1.0, end: 0.1).animate(
+        CurvedAnimation(parent: controller, curve: Interval(0.75, 1.0)));
 
     moverDerecha = Tween(begin: 0.0, end: 200.0)
         .animate(CurvedAnimation(parent: controller, curve: Curves.easeInBack));
@@ -81,7 +85,8 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>
           child: Transform.rotate(
             angle: rotacion.value,
             child: Opacity(
-              opacity: opacidad.value,
+              opacity:
+                  opacidad.value < 1.0 ? opacidad.value : opacidadOut.value,
               child: Transform.scale(
                   scale: agrandar.value, child: childRectangulo),
             ),
