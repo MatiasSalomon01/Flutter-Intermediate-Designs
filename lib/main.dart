562d479218222e5ee07b300694a9de97413c8370
=Matias Salomon <matias.salomon@clt.com.py>
2023-03-22 15:32:41 -0300

    Dark y Light Themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,4 @@
-import 'package:designs/src/challenges/cuadrado_animado_page.dart';
-import 'package:designs/src/labs/circular_progress_page.dart';
-import 'package:designs/src/pages/animate_do_pages.dart';
-import 'package:designs/src/pages/graficas_circulares_page.dart';
-import 'package:designs/src/labs/slideshow_page.dart';
-import 'package:designs/src/pages/emergency_page.dart';
 import 'package:designs/src/pages/launcher_page.dart';
-import 'package:designs/src/pages/navegacion_page.dart';
-import 'package:designs/src/pages/sliver_list_page.dart';
-import 'package:designs/src/pages/pinterest_page.dart';
-import 'package:designs/src/pages/slideshow_page.dart';
-import 'package:designs/src/pages/animaciones_page.dart';
-import 'package:designs/src/pages/headers_page.dart';
-import 'package:designs/src/pages/twitter_page.dart';
 import 'package:designs/src/theme/theme_changer.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -28,17 +15,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final appTheme = Provider.of<ThemeChanger>(context);
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Intermediate Designs',
-      // home: HeadersPage()
-      // home: AnimacionesPage()
-      // home: GraficasCircularesPage()
-      // home: PinterestPage()
-      // home: SlidesShowPage()
-      // home: EmergencyPage(),
-      // home: SliverListPage(),
-      // home: AnimateDoPage(),
+      theme: appTheme.currentTheme,
       home: LauncherPage(),
     );
   }
--- lib/src/theme/theme_changer.dart
+++ lib/src/theme/theme_changer.dart
@@ -3,19 +3,56 @@ import 'package:flutter/material.dart';
 class ThemeChanger extends ChangeNotifier {
   bool _darkTheme = false;
   bool _customTheme = false;
+  ThemeData? _currentTheme;
 
   bool get darkTheme => _darkTheme;
   bool get customTheme => _customTheme;
+  ThemeData? get currentTheme => _currentTheme;
+
+  // ThemeChanger(int theme) {
+  //   switch (theme) {
+  //     case 1:
+  //       _darkTheme = false;
+  //       _customTheme = false;
+  //       _currentTheme = ThemeData.light();
+  //       break;
+  //     case 2:
+  //       _darkTheme = true;
+  //       _customTheme = false;
+  //       _currentTheme = ThemeData.dark();
+  //       break;
+  //     case 3:
+  //       _darkTheme = false;
+  //       _customTheme = true;
+  //       break;
+  //     default:
+  //       _darkTheme = false;
+  //       _customTheme = false;
+  //       _currentTheme = ThemeData.light();
+  //   }
+  // }
 
   set darkTheme(bool value) {
     _customTheme = false;
     _darkTheme = value;
+    if (value) {
+      _currentTheme = ThemeData.dark();
+    } else {
+      _currentTheme = ThemeData.light();
+    }
     notifyListeners();
   }
 
   set customTheme(bool value) {
     _customTheme = value;
     _darkTheme = false;
+    if (value) {
+      _currentTheme = ThemeData.dark();
+    } else {
+      _currentTheme = ThemeData.light();
+    }
     notifyListeners();
   }
+
+  // set currentTheme(ThemeData value) {}
 }
