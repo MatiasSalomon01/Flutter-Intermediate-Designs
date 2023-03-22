7e990e5d68e1d7a0de984d2d78f6ca1dfac2371c
=Matias Salomon <matias.salomon@clt.com.py>
2023-03-22 11:53:27 -0300

    Todos los pages comentados y preparados para usar

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
-// import 'package:designs/src/challenges/cuadrado_animado_page.dart';
-// import 'package:designs/src/labs/circular_progress_page.dart';
-// import 'package:designs/src/pages/graficas_circulares_page.dart';
-// import 'package:designs/src/labs/slideshow_page.dart';
-// import 'package:designs/src/pages/emergency_page.dart';
+import 'package:designs/src/challenges/cuadrado_animado_page.dart';
+import 'package:designs/src/labs/circular_progress_page.dart';
+import 'package:designs/src/pages/graficas_circulares_page.dart';
+import 'package:designs/src/labs/slideshow_page.dart';
+import 'package:designs/src/pages/emergency_page.dart';
 import 'package:designs/src/pages/sliver_list_page.dart';
-// import 'package:designs/src/pages/pinterest_page.dart';
-// import 'package:designs/src/pages/slideshow_page.dart';
-// import 'package:designs/src/pages/animaciones_page.dart';
-// import 'package:designs/src/pages/headers_page.dart';
+import 'package:designs/src/pages/pinterest_page.dart';
+import 'package:designs/src/pages/slideshow_page.dart';
+import 'package:designs/src/pages/animaciones_page.dart';
+import 'package:designs/src/pages/headers_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -18,9 +18,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Intermediate Designs',
-        // home: HeadersPage());
-        home: SliverListPage());
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Intermediate Designs',
+      // home: HeadersPage()
+      // home: AnimacionesPage()
+      // home: GraficasCircularesPage()
+      // home: PinterestPage()
+      // home: SlidesShowPage()
+      // home: EmergencyPage(),
+      home: SliverListPage(),
+    );
   }
 }
