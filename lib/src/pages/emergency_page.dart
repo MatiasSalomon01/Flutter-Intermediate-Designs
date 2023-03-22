107e57c48788b7271a5282fcbd051e2c16919cc3
=Matias Salomon <matias.salomon@clt.com.py>
2023-03-22 08:47:07 -0300

    Información dentro del botón

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -9,6 +9,7 @@ class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: Colors.red,
       // body: PageHeader(),
       body: Center(
         child: BotonGordo(),
--- lib/src/widgets/boton_gordo.dart
+++ lib/src/widgets/boton_gordo.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BotonGordo extends StatelessWidget {
   const BotonGordo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return _BotonGordoBackground();
+    return Stack(
+      children: [
+        _BotonGordoBackground(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 140,
+              width: 40,
+            ),
+            FaIcon(
+              FontAwesomeIcons.carBurst,
+              color: Colors.white,
+              size: 40,
+            ),
+            SizedBox(width: 20),
+            Expanded(
+              child: Text(
+                'Murder Accidents',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+            FaIcon(
+              FontAwesomeIcons.chevronRight,
+              color: Colors.white,
+            ),
+            SizedBox(width: 40),
+          ],
+        )
+      ],
+    );
   }
 }
 
@@ -21,17 +52,38 @@ class _BotonGordoBackground extends StatelessWidget {
       width: double.infinity,
       height: 100,
       decoration: BoxDecoration(
-          // color: Colors.red,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.2),
-              offset: Offset(4, 6),
-              blurRadius: 10,
-            ),
+        // color: Colors.red,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            offset: Offset(4, 6),
+            blurRadius: 10,
+          ),
+        ],
+        borderRadius: BorderRadius.circular(15),
+        gradient: LinearGradient(
+          colors: [
+            Color(0xff6989f5),
+            Color(0xff906ef5),
           ],
-          borderRadius: BorderRadius.circular(15),
-          gradient:
-              LinearGradient(colors: [Color(0xff6989f5), Color(0xff906ef5)])),
+        ),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: Stack(
+          children: [
+            Positioned(
+              top: -20,
+              right: -20,
+              child: FaIcon(
+                FontAwesomeIcons.carBurst,
+                size: 150,
+                color: Colors.white.withOpacity(0.2),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
