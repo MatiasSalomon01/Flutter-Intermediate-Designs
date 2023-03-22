21d6dc34ce6d69d7606d90892ce58b1949221105
=Matias Salomon <matias.salomon@clt.com.py>
2023-03-22 10:43:02 -0300

    Diseño básico de la aplicación

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
 // import 'package:designs/src/labs/circular_progress_page.dart';
 // import 'package:designs/src/pages/graficas_circulares_page.dart';
 // import 'package:designs/src/labs/slideshow_page.dart';
-import 'package:designs/src/pages/emergency_page.dart';
+// import 'package:designs/src/pages/emergency_page.dart';
 import 'package:designs/src/pages/sliver_list_page.dart';
 // import 'package:designs/src/pages/pinterest_page.dart';
 // import 'package:designs/src/pages/slideshow_page.dart';
--- lib/src/pages/sliver_list_page.dart
+++ lib/src/pages/sliver_list_page.dart
@@ -5,9 +5,83 @@ class SliverListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('SliverListPage'),
+    return Scaffold(
+      // body: _ListaTareas(),
+      body: _Titulo(),
+    );
+  }
+}
+
+class _Titulo extends StatelessWidget {
+  const _Titulo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: 30),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+          child: Text(
+            'New',
+            style: TextStyle(color: Color(0xff532128), fontSize: 50),
+          ),
+        ),
+        Stack(
+          children: [
+            SizedBox(width: 100),
+            Positioned(
+              bottom: 8,
+              child: Container(
+                width: 150,
+                height: 8,
+                color: Color(0xfff7cdd5),
+              ),
+            ),
+            Container(
+              child: Text(
+                'List',
+                style: TextStyle(
+                    color: Color(0xffd93a30),
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class _ListaTareas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 20, itemBuilder: (context, index) => _ListItem());
+  }
+}
+
+class _ListItem extends StatelessWidget {
+  const _ListItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.all(10),
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        borderRadius: BorderRadius.circular(30),
+      ),
+      padding: EdgeInsets.all(10),
+      child: Text(
+        'Orange',
+        style: TextStyle(
+            color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
       ),
     );
   }
