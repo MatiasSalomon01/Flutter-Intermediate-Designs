73796f6c0318a3142dd29e2f89f2dcc90279a962
=Matias Salomon <matias.salomon@clt.com.py>
2023-03-23 14:04:45 -0300

    Fondo con gradiente

--- lib/src/MusicPlayer/pages/music_player_page.dart
+++ lib/src/MusicPlayer/pages/music_player_page.dart
@@ -8,15 +8,42 @@ class MusicPlayerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          CustomAppBar(),
-          ImagenDiscoDuracion(),
-          TituloPlay(),
-          Expanded(
-            child: Lyrics(),
-          )
+          Background(),
+          Column(
+            children: [
+              CustomAppBar(),
+              ImagenDiscoDuracion(),
+              TituloPlay(),
+              Expanded(child: Lyrics())
+            ],
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: screenSize.height * 0.8,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(60),
+        ),
+        gradient: LinearGradient(
+          begin: Alignment.centerLeft,
+          end: Alignment.center,
+          colors: [
+            Color(0xff333333e),
+            Color(0xff201e28),
+          ],
+        ),
       ),
     );
   }
