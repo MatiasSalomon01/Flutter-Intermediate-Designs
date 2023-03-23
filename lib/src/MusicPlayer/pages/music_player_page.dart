0b1d1bd65e668cdd3358308a020e5552340aef7a
=Matias Salomon <matias.salomon@clt.com.py>
2023-03-23 12:07:46 -0300

    Imagen del disco

--- lib/src/MusicPlayer/pages/music_player_page.dart
+++ lib/src/MusicPlayer/pages/music_player_page.dart
@@ -8,9 +8,72 @@ class MusicPlayerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        children: [CustomAppBar(), ImagesDIscoDuracion()],
+      ),
+    );
+  }
+}
+
+class ImagesDIscoDuracion extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
         children: [
-          CustomAppBar(),
+          //Disco
+          ImagesDisco()
+          //Barra de Progreso
         ],
+      ),
+    );
+  }
+}
+
+class ImagesDisco extends StatelessWidget {
+  const ImagesDisco({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      width: 250,
+      height: 250,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(200),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image(image: AssetImage('assets/images_music_player/aurora.jpg')),
+            Container(
+              width: 25,
+              height: 25,
+              decoration: BoxDecoration(
+                color: Colors.black38,
+                borderRadius: BorderRadius.circular(100),
+              ),
+            ),
+            Container(
+              width: 18,
+              height: 18,
+              decoration: BoxDecoration(
+                color: Color(0xff1c1c25),
+                borderRadius: BorderRadius.circular(100),
+              ),
+            )
+          ],
+        ),
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(200),
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          colors: [
+            Color(0xff484750),
+            Color(0xff1e1c24),
+          ],
+        ),
       ),
     );
   }
