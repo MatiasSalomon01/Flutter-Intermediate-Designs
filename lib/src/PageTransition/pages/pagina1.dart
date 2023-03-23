cf8c63c4965bfb6f755153a9fcb9e387d9335a6b
=Matias Salomon <matias.salomon@clt.com.py>
2023-03-23 15:56:53 -0300

    SlideTransition

--- lib/src/PageTransition/pages/pagina1.dart
+++ lib/src/PageTransition/pages/pagina1.dart
@@ -1,3 +1,4 @@
+import 'package:designs/src/PageTransition/pages/pagina2.dart';
 import 'package:flutter/material.dart';
 
 class Pagina1 extends StatelessWidget {
@@ -6,7 +7,6 @@ class Pagina1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
       appBar: AppBar(
         title: Text('Pagina 1'),
       ),
@@ -14,9 +14,27 @@ class Pagina1 extends StatelessWidget {
         child: Text('Pagina1'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(context, _crearRuta());
+        },
         child: Icon(Icons.access_time),
       ),
+    );
+  }
+
+  Route _crearRuta() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => Pagina2(),
+      // transitionDuration: Duration(milliseconds: 300),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        final curvedAnimation =
+            CurvedAnimation(parent: animation, curve: Curves.easeInOut);
+        return SlideTransition(
+          position: Tween(begin: Offset(0.5, 1.0), end: Offset.zero)
+              .animate(curvedAnimation),
+          child: child,
+        );
+      },
     );
   }
 }
